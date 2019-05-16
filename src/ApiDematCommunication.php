b51b763bf414941f7746f7ec0d83f354e52aefcc
Rémi Zanzi <r.zanzi@imanaging-document.fr>
2019-05-16 16:47:19 +0200

    Prise en compte du timeout dans les requetes APIDEMAT

--- src/ApiDematCommunication.php
+++ src/ApiDematCommunication.php
@@ -61,7 +61,7 @@ class ApiDematCommunication extends ImanagingApiCommunication
 
       $requestResult->setGlobalUrl($globalUrl);
 
-      $result = $this->sendRequest($globalUrl, $url, $postMode, $postData, $this->mock_enable_on_dev_env);
+      $result = $this->sendRequest($globalUrl, $url, $postMode, $postData, $this->mock_enable_on_dev_env, $this->timeout);
 
       $requestResult->setHttpCode($result['http_code']);
       $requestResult->setData($result['response']);
